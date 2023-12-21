1da71a47999d7363d4510e5166af28eaf2015e96
maazsurti <maaz@rawcodev.com>
2023-12-21 22:23:11 +0530

    update

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, "0.0.0.0", () => {
+app.listen(PORT, () => {
     console.log(`Connected at port ${PORT}`)
 })
 
